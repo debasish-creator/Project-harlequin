6d4d58b9f6a31979daa6827ec76a6fb8c530adc6
deb2000-sudo <59384249+deb2000-sudo@users.noreply.github.com>
2021-02-10 09:00:23 +0530

    footer in all pages updated
    in every page footer is updated

--- Backendfooter.php
+++ Backendfooter.php
@@ -1,17 +1,87 @@
 <?php Confirm_Login(); ?>
+<head>
+    <title>Backend footer</title>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
 <!--FOOTER STARTS-->
 <div style="height: 5px; background: cornflowerblue"></div>
-<footer class="bg-dark text-white">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <p class="lead text-center">THINK HARD  |  <span id="year"></span>&copy: -----All right reserved</p>
-            </div>
+<footer class="footer-distributed" id="contact">
+    <div class="footer-left">
+        <h3>MindSaga <span id="year"></span></h3>
+        <p class="footer-links">
+            <a href="#" class="nav-link">About Us</a>
+            ·
+            <a href="index.php" class="nav-link">Blog</a>
+            ·
+            <a href="#" class="nav-link">Contact Us</a>
+            ·
+            <a href="#" class="nav-link">Features</a>
+            .
+        </p>
+        <p class="footer-company-name">Made with 💕 by Webwiz</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>Loremlaudantium assumenda</span> Odisha, India</p>
         </div>
+
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>random789</p>
+        </div>
+
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="">webwiz.nitrkl@gmail.com</a></p>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+
+        <p class="footer-company-about">
+            <span>About MindSaga</span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni expedita assumenda temporibus sapiente veritatis veniam
+        </p>
+        <ul class="social">
+            <li>
+                <a href="#" class="tw" title="Tweet this page!">
+                    <i class="icon-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="fb" title="Share this page!">
+                    <i class="icon-facebook"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="gp" title="Share this page!">
+                    <i class="icon-google-plus"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="gh" title="Tweet this page!">
+                    <i class="icon-github"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="insta" title="Tweet this page!">
+                    <i class="icon-instagram"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="in" title="Tweet this page!">
+                    <i class="icon-linkedin"></i>
+                </a>
+            </li>
+        </ul>
     </div>
 </footer>
-<!--FOOTER ENDS-->
 <div style="height: 5px; background: cornflowerblue"></div>
+<!----FOOTER ENDS---->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
--- Login.php
+++ Login.php
@@ -42,6 +42,7 @@ if(isset($_POST["Submit"])){
     <script src="https://kit.fontawesome.com/7f6ee3d237.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/footer.css">
     <title>Admin Login</title>
 </head>
 <body>
@@ -56,15 +57,7 @@ if(isset($_POST["Submit"])){
 <!--NAVIGATION BAR ENDS-->
 
 <!--HEADER STARTS-->
-<header class="bg-dark text-white py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-            <h1></h1>
-            </div>
-        </div>
-    </div>
-</header>
+
 <!--HEADER ENDS-->
 
 <!--MAIN AREA STARTS-->
@@ -104,14 +97,96 @@ if(isset($_POST["Submit"])){
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
-
 </section>
-
-
 <br><br><br><br><br><br>
 
 <!--MAIN AREA ENDS-->
-<?php require_once ("footer.php");?>
+<!--FOOTER STARTS-->
+<div style="height: 5px; background: cornflowerblue"></div>
+<footer class="footer-distributed" id="contact">
+    <div class="footer-left">
+        <h3>MindSaga <span id="year"></span></h3>
+        <p class="footer-links">
+            <a href="#" class="nav-link">About Us</a>
+            ·
+            <a href="index.php" class="nav-link">Blog</a>
+            ·
+            <a href="#" class="nav-link">Contact Us</a>
+            ·
+            <a href="#" class="nav-link">Features</a>
+            .
+        </p>
+        <p class="footer-company-name">Made with 💕 by Webwiz</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>Loremlaudantium assumenda</span> Odisha, India</p>
+        </div>
+
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>random789</p>
+        </div>
+
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="">webwiz.nitrkl@gmail.com</a></p>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+
+        <p class="footer-company-about">
+            <span>About MindSaga</span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni expedita assumenda temporibus sapiente veritatis veniam
+        </p>
+        <ul class="social">
+            <li>
+                <a href="#" class="tw" title="Tweet this page!">
+                    <i class="icon-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="fb" title="Share this page!">
+                    <i class="icon-facebook"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="gp" title="Share this page!">
+                    <i class="icon-google-plus"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="gh" title="Tweet this page!">
+                    <i class="icon-github"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="insta" title="Tweet this page!">
+                    <i class="icon-instagram"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="in" title="Tweet this page!">
+                    <i class="icon-linkedin"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</footer>
+<div style="height: 5px; background: cornflowerblue"></div>
+<!----FOOTER ENDS---->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script>
+    $('#year').text(new Date().getFullYear());
+</script>
+</body>
+</html>
