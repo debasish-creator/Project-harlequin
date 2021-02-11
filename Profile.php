3362db4dc3ba8219f5611774e0f4248f774fe65e
deb2000-sudo <59384249+deb2000-sudo@users.noreply.github.com>
2021-02-11 20:34:08 +0530

    php code updated

--- Profile.php
+++ Profile.php
@@ -34,11 +34,11 @@ if( $Result==1 ){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/footer.css">
     <title>Profile</title>
 </head>
 <body>
 <!--NAVIGATION BAR STARTS-->
-<div style="height: 10px; background: cornflowerblue"></div>
     <div class="navbar navbar-expand-lg navbar-light bg-custom">
         <div class="container">
             <a href="#" class="navbar-brand " style= "color:aliceblue; font-family: mindsagacustom;">MindSaga</a>
@@ -75,7 +75,6 @@ if( $Result==1 ){
             </div>
         </div>
     </div>
-<div style="height: 10px; background: cornflowerblue"></div>
 <!--NAVIGATION BAR ENDS-->
 
 <!--HEADER STARTS-->
@@ -105,4 +104,81 @@ if( $Result==1 ){
     </div>
 </section>
 <!--FOOTER STARTS-->
-<?php require_once ("footer.php");?>
+<div style="height: 5px; background: cornflowerblue"></div>
+<footer class="footer-distributed" id="contact">
+    <div class="footer-left">
+        <h3>MindSaga <span id="year"></span></h3>
+        <p class="footer-links">
+            <a href="#" class="nav-link" style="margin-left: -15px;">About Us</a>
+            ·
+            <a href="index.php" class="nav-link">Blog</a>
+            ·
+            <a href="#" class="nav-link">Contact Us</a>
+            ·
+            <a href="#" class="nav-link">Features</a>
+            .
+        </p>
+        <p class="footer-company-name">Made with 💕 by Webwiz</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="fa fa-map-marker" ></i>
+            <p><span>Loremlaudantium assumenda</span> Odisha, India</p>
+        </div>
+
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>random789</p>
+        </div>
+
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="">webwiz.nitrkl@gmail.com</a></p>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+
+        <p class="footer-company-about">
+            <span>About MindSaga</span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni expedita assumenda temporibus sapiente veritatis veniam
+        </p>
+        <ul class="social">
+            <li>
+                <a href="#" class="fa fa-twitter"></a>
+            </li>
+            <li>
+                <a href="#" class="fa fa-facebook"></a>
+            </li>
+            <li>
+                <a href="#" class="fa fa-google"></a>
+            </li>
+            <li>
+                <a href="#" class="fa fa-github"></a>
+            </li>
+            <li>
+                <a href="#" class="fa fa-instagram"></a>
+            </li>
+            <li>
+                <a href="#" class="fa fa-linkedin"></a>
+            </li>
+        </ul>
+    </div>
+</footer>
+<div style="height: 5px; background: cornflowerblue"></div>
+<!----FOOTER ENDS---->
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+<script>
+    $('#year').text(new Date().getFullYear());
+</script>
+</body>
+</html>
