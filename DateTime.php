6b0070d13a5ab1d7a2802475913ee5857a9ffb48
amyra98 <75852935+amyra98@users.noreply.github.com>
2021-02-22 18:15:45 +0530

    updating

--- DateTime.php
+++
@@ -1,7 +0,0 @@
-<?php Confirm_Login(); ?>
-<?php
-date_default_timezone_set("Asia/Kolkata");
-$CurrentTime=time();
-$DateTime=strftime("%B-%d-%Y %H: %M: %S",$CurrentTime);
-echo $DateTime;
-?>
--- WOC_IIITKalyani/Timeline.md
+++
@@ -1,5 +0,0 @@
-4th Feb -Ideation about layout and ui will be completed
-12 Feb-Frontend work will be completed
-
-then final testing and submission of final project
-
